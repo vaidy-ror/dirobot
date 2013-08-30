44d0a652b4601cea153275283ddaf978502a552a
Vaidy <vaidy.vaidyanathan@gmail.com>
2013-08-29 22:46:21 -0700

    added indices generation logic

--- diutilsv10/diutils/autocomplete_indices/autocomplete_indices.py
+++ diutilsv10/diutils/autocomplete_indices/autocomplete_indices.py
@@ -7,20 +7,41 @@ pip_list_filename="pip_list.txt"
 
 try:
     shutil.rmtree('robot-indices')
-    print 'old indices dir removed'
+    print 'old robot-indices dir removed'
 except:
-    print 'no indices dir found'
+    print 'robot-indices dir not found.  nothing to cleanup.'
 
-print "creating pip list file"
-os.system("pip list > %s"  %   pip_list_filename)
-print "pip list file created"
+print "creating robot-indices directory"
+os.mkdir("robot-indices")
 
-f = open(pip_list_filename, 'r')
-for line in f:
-    print "sssss %s" %  line
+modules = [
+           "Selenium2Library",
+           "Rammbock",
+           "AndroidLibrary",
+           "HttpLibrary",
+           "RequestsLibrary",           
+           ]
+
+for module in modules:
+    os.system("python -m robot.libdoc %s list > robot-indices/%s.index"     %   (module,module))
+    
 
 
 
 
-print "cleaning up pip list file"
-os.remove(pip_list_filename)
+
+
+
+
+
+
+
+# print "creating pip list file"
+# os.system("pip list > %s"  %   pip_list_filename)
+# print "pip list file created"
+# 
+# f = open(pip_list_filename, 'r')
+# for line in f:
+#     print "sssss %s" %  line
+# print "cleaning up pip list file"
+# os.remove(pip_list_filename)
