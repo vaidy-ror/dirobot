b19303b12e0db8f3a848ad068cacce806ca5afa6
Vaidy <vaidy.vaidyanathan@gmail.com>
2013-08-29 21:35:36 -0700

    adding setup file

---
+++ dilibsv10/setup.py
@@ -0,0 +1,25 @@
+__author__ = 'vshanmugam'
+
+from setuptools import setup
+
+setup(name='dilibs',
+      version='1.0',
+      description='di libraries',
+      url='https://github.com/vaidy-ror/dirobot.git',
+      author='vaidyanathan shanmugam',
+      author_email='vaidyanathan_shanmugam@intuit.com',
+      license='',
+      packages=['dilibs',],
+      install_requires = [
+                           'robotframework-sudslibrary==0.7'
+                          'robotframework-selenium2library==1.3.0'
+                          'robotframework-rammbock==0.3.0'
+                          'robotframework-databaselibrary==0.6'
+                          'robotframework-androidlibrary==0.1.19'
+                          'robotframework-httplibrary==0.4.2',
+                          'robotframework-requests==0.3.3',
+                          'robotframework==2.8.1',
+                          'requests==1.2.3',
+                          'dipipinstall==1.0'
+                          ],
+      zip_safe=False)
